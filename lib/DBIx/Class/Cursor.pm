1923c0b4b27dfc62e61cc02b6391e4d37349bcfb
Matt S Trout <mst@shadowcat.co.uk>
2005-07-31 22:25:34 +0000

    Cursor.pm because I'm an idiot and forgot to svk add it last time

---
+++ lib/DBIx/Class/Cursor.pm
@@ -0,0 +1,53 @@
+package DBIx::Class::Cursor;
+
+use strict;
+use warnings;
+use overload
+        '0+'     => 'count',
+        fallback => 1;
+
+sub new {
+  my ($it_class, $db_class, $sth, $args, $cols) = @_;
+  $sth->execute(@{$args || []}) unless $sth->{Active};
+  my $new = {
+    class => $db_class,
+    sth => $sth,
+    cols => $cols,
+    args => $args };
+  return bless ($new, $it_class);
+}
+
+sub next {
+  my ($self) = @_;
+  my @row = $self->{sth}->fetchrow_array;
+  return unless @row;
+  #unless (@row) { $self->{sth}->finish; return; }
+  return $self->{class}->_row_to_object($self->{cols}, \@row);
+}
+
+sub count {
+  return scalar $_[0]->all; # So inefficient
+}
+
+sub all {
+  my ($self) = @_;
+  $self->reset;
+  my @all;
+  while (my $obj = $self->next) {
+    push(@all, $obj);
+  }
+  $self->reset;
+  return @all;
+}
+
+sub reset {
+  $_[0]->{sth}->finish if $_[0]->{sth}->{Active};
+  $_[0]->{sth}->execute(@{$_[0]->{args} || []});
+  return $_[0];
+}
+
+sub first {
+  return $_[0]->reset->next;
+}
+
+1;
