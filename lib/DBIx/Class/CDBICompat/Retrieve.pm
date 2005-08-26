19e3ec11e0527341c483fff1a79b7f16dc6e15c2
Andy Grundman <andy@hybridized.org>
2005-08-26 01:43:30 +0000

    Fixed bug where retrieve_all ignored options such as order_by

--- lib/DBIx/Class/CDBICompat/Retrieve.pm
+++ lib/DBIx/Class/CDBICompat/Retrieve.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings FATAL => 'all';
 
 sub retrieve          { shift->find(@_)            }
-sub retrieve_all      { shift->search_literal('1') }
+sub retrieve_all      { shift->search_literal('1', @_) }
 sub retrieve_from_sql { shift->search_literal(@_)  }
 
 sub count_all          { shift->count_literal('1')  }
