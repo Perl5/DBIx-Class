90e6de6c3dac0089fa0cea83db3251f3c6ca393d
Matt S Trout <mst@shadowcat.co.uk>
2005-12-07 01:37:55 +0000

    Classdata test from abraxxa

--- t/lib/DBICTest/Schema/Artist.pm
+++ t/lib/DBICTest/Schema/Artist.pm
@@ -6,4 +6,9 @@ DBICTest::Schema::Artist->table('artist');
 DBICTest::Schema::Artist->add_columns(qw/artistid name/);
 DBICTest::Schema::Artist->set_primary_key('artistid');
 
+__PACKAGE__->mk_classdata('field_name_for', {
+    artistid    => 'primary key',
+    name        => 'artist name',
+});
+
 1;
