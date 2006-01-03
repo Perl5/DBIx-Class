95da6f35d076191b29ef0e9575e2a477b2e29e52
Matt S Trout <mst@shadowcat.co.uk>
2006-01-03 18:17:44 +0000

    Added note about version padding to remind people packaging dists

--- lib/DBIx/Class.pm
+++ lib/DBIx/Class.pm
@@ -9,6 +9,10 @@ use base qw/DBIx::Class::Componentised Class::Data::Accessor/;
 sub mk_classdata { shift->mk_classaccessor(@_); }
 sub component_base_class { 'DBIx::Class' }
 
+# Always remember to do all digits for the version even if they're 0
+# i.e. first release of 0.XX *must* be 0.XX000. This avoids fBSD ports
+# brain damage and presumably various other packaging systems too
+
 $VERSION = '0.04999_02';
 
 1;
