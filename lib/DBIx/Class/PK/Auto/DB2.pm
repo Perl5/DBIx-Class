f6a3f5ae498091a233c8a96cf3a1b262ce0c5e38
Jess Robinson <castaway@desert-island.me.uk>
2006-01-08 16:45:46 +0000

    New DB2 Automagic primary key collecting

---
+++ lib/DBIx/Class/PK/Auto/DB2.pm
@@ -0,0 +1,22 @@
+package DBIx::Class::PK::Auto::DB2;
+
+use strict;
+use warnings;
+
+use base qw/DBIx::Class/;
+
+__PACKAGE__->load_components(qw/PK::Auto/);
+
+sub last_insert_id
+{
+    my ($self) = @_;
+
+    my $dbh = $self->storage->dbh;
+    my $sth = $dbh->prepare_cached("VALUES(IDENTITY_VAL_LOCAL())");
+    $sth->execute();
+
+    my @res = $sth->fetchrow_array();
+
+    return @res ? $res[0] : undef;
+                         
+}
