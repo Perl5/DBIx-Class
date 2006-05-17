445e5e31324c54ce3ffe96199e4e308efaa0823e
Matt S Trout <mst@shadowcat.co.uk>
2006-05-17 00:22:06 +0000

    Missing stuff for DateTime branch

---
+++ t/lib/DBICTest/Schema/Event.pm
@@ -0,0 +1,16 @@
+package DBICTest::Schema::Event;
+
+use base qw/DBIx::Class/;
+
+__PACKAGE__->load_components(qw/InflateColumn::DateTime PK::Auto Core/);
+
+__PACKAGE__->table('event');
+
+__PACKAGE__->add_columns(
+  id => { data_type => 'integer', is_auto_increment => 1 },
+  starts_at => { data_type => 'datetime' }
+);
+
+__PACKAGE__->set_primary_key('id');
+
+1;
