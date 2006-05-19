645670f5d654667e543b5ee86ae4fb6436aa389b
Jess Robinson <castaway@desert-island.me.uk>
2006-05-19 08:38:59 +0000

    Remove Versioning.pm, add backup to SQLite

--- lib/DBIx/Class/Storage/DBI/SQLite.pm
+++ lib/DBIx/Class/Storage/DBI/SQLite.pm
@@ -2,11 +2,42 @@ package DBIx::Class::Storage::DBI::SQLite;
 
 use strict;
 use warnings;
+use POSIX 'strftime';
+use File::Copy;
+use Path::Class;
 
 use base qw/DBIx::Class::Storage::DBI::MultiDistinctEmulation/;
 
 sub last_insert_id {
   return $_[0]->dbh->func('last_insert_rowid');
+}
+
+sub backup
+{
+  my ($self) = @_;
+
+  ## Where is the db file?
+  my $dsn = $self->connect_info()->[0];
+
+  my $dbname = $1 if($dsn =~ /dbname=([^;]+)/);
+  if(!$dbname)
+  {
+    $dbname = $1 if($dsn =~ /^dbi:SQLite:(.+)$/i);
+  }
+  $self->throw_exception("Cannot determine name of SQLite db file") 
+    if(!$dbname || !-f $dbname);
+
+  print "Found database: $dbname\n";
+  my $dbfile = file($dbname);
+#  my ($vol, $dir, $file) = File::Spec->splitpath($dbname);
+  my $file = $dbfile->basename();
+  $file = strftime("%y%m%d%h%M%s", localtime()) . $file; 
+  $file = "B$file" while(-f $file);
+  
+  my $res = copy($dbname, $file);
+  $self->throw_exception("Backup failed! ($!)") if(!$res);
+
+  return $file;
 }
 
 1;
