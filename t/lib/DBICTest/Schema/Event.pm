45fa82889e637f9382eb2890b44fc58ef122de25
Matt S Trout <mst@shadowcat.co.uk>
2006-05-20 00:33:14 +0000

    added datetime parser types for the dbs I can find them for

--- t/lib/DBICTest/Schema/Event.pm
+++ t/lib/DBICTest/Schema/Event.pm
@@ -2,15 +2,4 @@ package DBICTest::Schema::Event;
 
 use base qw/DBIx::Class/;
 
-__PACKAGE__->load_components(qw/InflateColumn::DateTime PK::Auto Core/);
-
-__PACKAGE__->table('event');
-
-__PACKAGE__->add_columns(
-  id => { data_type => 'integer', is_auto_increment => 1 },
-  starts_at => { data_type => 'datetime' }
-);
-
-__PACKAGE__->set_primary_key('id');
-
-1;
+__PACKAGE__->load_components(qw/
