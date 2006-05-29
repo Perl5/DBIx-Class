0b375b6836266ad5ac353154ec3d58147a5581b8
Aran Deltac <bluefeet@cpan.org>
2006-05-29 05:49:45 +0000

    POD for InflateColumn::DateTime.

--- lib/DBIx/Class/InflateColumn/DateTime.pm
+++ lib/DBIx/Class/InflateColumn/DateTime.pm
@@ -4,6 +4,35 @@ use strict;
 use warnings;
 use base qw/DBIx::Class/;
 
+=head1 NAME
+
+DBIx::Class::InflateColumn::DateTime - Auto-create DateTime objects from datetime columns.
+
+=head1 SYNOPSIS
+
+Load this component and then declare one or more 
+columns to be of the datetime datatype.
+
+  package Event;
+  __PACKAGE__->load_components(qw/InflateColumn::DateTime/);
+  __PACKAGE__->add_columns(
+    starts_when => { data_type => 'datetime' }
+  );
+
+Then you can treat the specified column as a L<DateTime> object.
+
+  print "This event starts the month of ".
+    $event->starts_when->month_name();
+
+=head1 DESCRIPTION
+
+This module figures out the type of DateTime::Format::* class to 
+inflate/deflate with based on the type of DBIx::Class::Storage::DBI::* 
+that you are using.  If you switch from one database to a different 
+one your code will continue to work without modification.
+
+=cut
+
 __PACKAGE__->load_components(qw/InflateColumn/);
 
 __PACKAGE__->mk_group_accessors('simple' => '__datetime_parser');
@@ -38,3 +67,26 @@ sub _datetime_parser {
 }
 
 1;
+__END__
+
+=head1 SEE ALSO
+
+=over 4
+
+=item More information about the add_columns method, and column metadata, 
+      can be found in the documentation for L<DBIx::Class::ResultSource>.
+
+=back
+
+=head1 AUTHOR
+
+Matt S. Trout <mst@shadowcatsystems.co.uk>
+
+=head1 CONTRIBUTORS
+
+Aran Deltac <bluefeet@cpan.org>
+
+=head1 LICENSE
+
+You may distribute this code under the same terms as Perl itself.
+
