9eb328922da470ed2053fdf8369f986e842fc226
Robert Sedlacek <phaylon@dunkelheit.at>
2006-06-01 15:16:18 +0000

    Test if value is blessed before ->isa

--- lib/DBIx/Class/Relationship/Base.pm
+++ lib/DBIx/Class/Relationship/Base.pm
@@ -3,6 +3,7 @@ package DBIx::Class::Relationship::Base;
 use strict;
 use warnings;
 
+use Scalar::Util ();
 use base qw/DBIx::Class/;
 
 =head1 NAME
@@ -339,7 +340,7 @@ sub set_from_related {
   if (defined $f_obj) {
     my $f_class = $self->result_source->schema->class($rel_obj->{class});
     $self->throw_exception( "Object $f_obj isn't a ".$f_class )
-      unless $f_obj->isa($f_class);
+      unless Scalar::Util::blessed($f_obj) and $f_obj->isa($f_class);
   }
   $self->set_columns(
     $self->result_source->resolve_condition(
