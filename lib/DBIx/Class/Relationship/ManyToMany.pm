3a868fb29b0bb50a7216f770cc9a71d358d67431
Justin Guenther <jguenther@cpan.org>
2006-06-05 20:04:10 +0000

    Added preliminary tests for new helpers

--- lib/DBIx/Class/Relationship/ManyToMany.pm
+++ lib/DBIx/Class/Relationship/ManyToMany.pm
@@ -13,7 +13,9 @@ sub many_to_many {
     *{"${class}::${meth}"} = sub {
       my $self = shift;
       my $attrs = @_ > 1 && ref $_[$#_] eq 'HASH' ? pop(@_) : {};
-      $self->search_related($rel)->search_related($f_rel, @_ > 0 ? @_ : undef, { %{$rel_attrs||{}}, %$attrs });
+      $self->search_related($rel)->search_related(
+        $f_rel, @_ > 0 ? @_ : undef, { %{$rel_attrs||{}}, %$attrs }
+      );
     };
 
     *{"${class}::add_to_${meth}"} = sub {
