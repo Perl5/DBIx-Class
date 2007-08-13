8e766a1348ff3238d605a8126419e7d862039014
Tom Hukins <tom@eborcom.com>
2007-08-13 09:42:16 +0000

    Fix a package name I corrected wrongly in revision 3666.

--- lib/DBIx/Class/Storage/DBI/SQLite.pm
+++ lib/DBIx/Class/Storage/DBI/SQLite.pm
@@ -50,7 +50,7 @@ sub backup
 
 =head1 NAME
 
-DBIx::Class::Storage::SQLite - Automatic primary key class for SQLite
+DBIx::Class::Storage::DBI::SQLite - Automatic primary key class for SQLite
 
 =head1 SYNOPSIS
 
