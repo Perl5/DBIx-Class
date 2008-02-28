3b7f3eac72dbf39bb9a616ae4884e51f6d872c26
Ash Berlin <ash_github@firemirror.com>
2008-02-28 19:33:59 +0000

    Todo tests for txn_rollback and scope_guard

--- lib/DBIx/Class/Storage/TxnScopeGuard.pm
+++ lib/DBIx/Class/Storage/TxnScopeGuard.pm
@@ -20,7 +20,23 @@ sub commit {
 sub DESTROY {
   my ($dismiss, $storage) = @{$_[0]};
 
-  $storage->txn_rollback unless $dismiss;
+  return if $dismiss;
+
+  my $exception = $@;
+
+  $DB::single = 1;
+
+  local $@;
+  eval { $storage->txn_rollback };
+  my $rollback_exception = $@;
+  if($rollback_exception) {
+    my $exception_class = "DBIx::Class::Storage::NESTED_ROLLBACK_EXCEPTION";
+
+    $storage->throw_exception(
+      "Transaction aborted: ${exception}. "
+      . "Rollback failed: ${rollback_exception}"
+    ) unless $rollback_exception =~ /$exception_class/;
+  }
 }
 
 1;
