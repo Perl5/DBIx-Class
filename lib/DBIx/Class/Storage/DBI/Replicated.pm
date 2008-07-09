79a77af646d55bab4840d883527d4f73c9426427
John Napiorkowski <jjn1056@yahoo.com>
2008-07-09 17:51:26 +0000

    removed the mistaken debug code

--- lib/DBIx/Class/Storage/DBI/Replicated.pm
+++ lib/DBIx/Class/Storage/DBI/Replicated.pm
@@ -424,14 +424,6 @@ sub execute_reliably {
     $self->throw_exception('Second argument must be a coderef');
   }
   
-    my $t0 = new Benchmark;
-    my $clone = $self->clone;
-    my $t1 = new Benchmark;
-    my $td = timediff($t1, $t0);
-    warn "----------------- the code took:",timestr($td),"\n";
-      
-
-  
   ##Get copy of master storage
   my $master = $self->master;
   
