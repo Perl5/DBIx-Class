2c69732158b0e65bcccfe6c2b9912082c62dd90e
Nigel Metheringham <nigelm@cpan.org>
2008-10-26 11:59:35 +0000

    Modified example to use load_namespace with default naming rather than load_classes

--- t/examples/Schema/MyDatabase/Main/Result/Artist.pm
+++ t/examples/Schema/MyDatabase/Main/Result/Artist.pm
@@ -1,10 +1,10 @@
-package MyDatabase::Main::Artist;
+package MyDatabase::Main::Result::Artist;
 use base qw/DBIx::Class/;
 __PACKAGE__->load_components(qw/PK::Auto Core/);
 __PACKAGE__->table('artist');
 __PACKAGE__->add_columns(qw/ artistid name /);
 __PACKAGE__->set_primary_key('artistid');
-__PACKAGE__->has_many('cds' => 'MyDatabase::Main::Cd');
+__PACKAGE__->has_many('cds' => 'MyDatabase::Main::Result::Cd');
 
 1;
 
