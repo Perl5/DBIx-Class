a085381e61284d46d374ada169f52ae6e35fc9bb
Peter Rabbitson <ribasushi@cpan.org>
2008-11-09 15:27:40 +0000

    SQLT behaves sanely in scalar context, no need for stupid version detections (ribasushi--)

--- maint/gen-schema.pl
+++ maint/gen-schema.pl
@@ -7,13 +7,5 @@ use lib qw(lib t/lib);
 use DBICTest::Schema;
 use SQL::Translator;
 
-my $sql_join_str = '';
-if (SQL::Translator->VERSION >= 0.09001) {
-    $sql_join_str .= ";";
-}
-if (SQL::Translator->VERSION >= 0.09) {
-    $sql_join_str .= "\n";
-}
-
 my $schema = DBICTest::Schema->connect;
-print join ($sql_join_str,$schema->storage->deployment_statements($schema, 'SQLite') );
+print scalar ($schema->storage->deployment_statements($schema, 'SQLite'));
