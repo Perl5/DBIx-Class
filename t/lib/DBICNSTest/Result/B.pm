0e6c5d584d334aa157caf244e062b4a24c19090c
Matt S Trout <mst@shadowcat.co.uk>
2009-01-09 03:28:44 +0000

    fixup source registration bugs where schema-specific things were being set on class' result_source_instance objects

--- t/lib/DBICNSTest/Result/B.pm
+++ t/lib/DBICNSTest/Result/B.pm
@@ -1,5 +1,5 @@
 package DBICNSTest::Result::B;
-use base qw/DBICNSTest::Result::A/;
+use base qw/DBIx::Class/;
 __PACKAGE__->load_components(qw/PK::Auto Core/);
 __PACKAGE__->table('b');
 __PACKAGE__->add_columns('b');
