d6aed6385781443c0a65cacabaa3a4f071d63f88
Rob Kinyon <rkinyon@cpan.org>
2009-07-10 22:03:07 +0000

    Applied patch from kados regarding use of a DateTime::Format class to validate

--- lib/DBIx/Class/InflateColumn/DateTime.pm
+++ lib/DBIx/Class/InflateColumn/DateTime.pm
@@ -51,6 +51,14 @@ It's also possible to explicitly skip inflation:
     starts_when => { data_type => 'datetime', inflate_datetime => 0 }
   );
 
+NOTE: Don't rely on C<InflateColumn::DateTime> to validate user input, this
+may have unexpected security implications. Instead, use an input parser to
+create a DateTime object. For instance, if your user input comes as a
+'YYYY-MM-DD' string, you can use C<DateTime::Format::ISO8601> thusly:
+
+  use DateTime::Format::ISO8601;
+  my $dt = DateTime::Format::ISO8601->parse_datetime('YYYY-MM-DD');
+
 =head1 DESCRIPTION
 
 This module figures out the type of DateTime::Format::* class to 
