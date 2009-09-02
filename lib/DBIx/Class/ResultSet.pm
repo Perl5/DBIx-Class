0e4fb877c0d647259a7b7f1f38f9dbe65b31cf6b
Peter Rabbitson <ribasushi@cpan.org>
2009-09-02 09:31:50 +0000

    Some cleanup... don't remember where it came from

--- lib/DBIx/Class/ResultSet.pm
+++ lib/DBIx/Class/ResultSet.pm
@@ -2853,7 +2853,7 @@ sub _resolved_attrs {
 
   if ( $attrs->{join} || $attrs->{prefetch} ) {
 
-    $self->throw_exception ('join/prefetch can not be used with a literal scalarref {from}')
+    $self->throw_exception ('join/prefetch can not be used with a custom {from}')
       if ref $attrs->{from} ne 'ARRAY';
 
     my $join = delete $attrs->{join} || {};
@@ -2998,6 +2998,13 @@ sub _rollout_hash {
 
 sub _calculate_score {
   my ($self, $a, $b) = @_;
+
+  if (defined $a xor defined $b) {
+    return 0;
+  }
+  elsif (not defined $a) {
+    return 1;
+  }
 
   if (ref $b eq 'HASH') {
     my ($b_key) = keys %{$b};
