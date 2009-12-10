3b5769af35e53f0c36c56787dab912064f1d0ce2
Arthur Axel "fREW" Schmidt <frioux@gmail.com>
2009-12-10 15:04:37 +0000

    release 0.08115

--- lib/DBIx/Class.pm
+++ lib/DBIx/Class.pm
@@ -24,7 +24,7 @@ sub component_base_class { 'DBIx::Class' }
 # Always remember to do all digits for the version even if they're 0
 # i.e. first release of 0.XX *must* be 0.XX000. This avoids fBSD ports
 # brain damage and presumably various other packaging systems too
-$VERSION = '0.08114';
+$VERSION = '0.08115';
 
 $VERSION = eval $VERSION; # numify for warning-free dev releases
 
