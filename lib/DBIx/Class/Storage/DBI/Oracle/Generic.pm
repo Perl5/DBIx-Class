a4433d8e55074f96911ede2a922fced8b96743de
Fabien Wernli <cpan@faxm0dem.org>
2010-02-14 08:49:44 +0000

    oracle_version

--- lib/DBIx/Class/Storage/DBI/Oracle/Generic.pm
+++ lib/DBIx/Class/Storage/DBI/Oracle/Generic.pm
@@ -25,20 +25,6 @@ versions before 9.
 
 =cut
 
-my %ora_reserved = map { $_, 1 } qw(
-    ACCESS ADD ALL ALTER AND ANY AS ASC AUDIT BETWEEN BY CHAR CHECK
-    CLUSTER COLUMN COMMENT COMPRESS CONNECT CREATE CURRENT DATE DECIMAL
-    DEFAULT DELETE DESC DISTINCT DROP ELSE EXCLUSIVE EXISTS FILE FLOAT
-    FOR FROM GRANT GROUP HAVING IDENTIFIED IMMEDIATE IN INCREMENT
-    INDEX INITIAL INSERT INTEGER INTERSECT INTO IS LEVEL LIKE LOCK
-    LONG MAXEXTENTS MINUS MLSLABEL MODE MODIFY NOAUDIT NOCOMPRESS NOT
-    NOWAIT NULL NUMBER OF OFFLINE ON ONLINE OPTION OR ORDER PCTFREE
-    PRIOR PRIVILEGES PUBLIC RAW RENAME RESOURCE REVOKE ROW ROWID ROWNUM
-    ROWS SELECT SESSION SET SHARE SIZE SMALLINT START SUCCESSFUL SYNONYM
-    SYSDATE TABLE THEN TO TRIGGER UID UNION UNIQUE UPDATE USER VALIDATE
-    VALUES VARCHAR VARCHAR2 VIEW WHENEVER WHERE WITH
-);
-
 use base qw/DBIx::Class::Storage::DBI/;
 use mro 'c3';
 
@@ -50,6 +36,10 @@ sub deployment_statements {
 	my $quote_char = $self->schema->storage->{'_sql_maker_opts'}->{'quote_char'};
 	$sqltargs->{quote_table_names} = 0 unless $quote_char;
 	$sqltargs->{quote_field_names} = 0 unless $quote_char;
+
+	my $oracle_version = eval { $self->_get_dbh->get_info(18) };
+
+  $sqltargs->{producer_args}{oracle_version} = $oracle_version;
 
 	$self->next::method($schema, $type, $version, $dir, $sqltargs, @rest);
 }
