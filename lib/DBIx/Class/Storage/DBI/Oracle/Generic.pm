cc42fa9ac80f921b0f835fbd3bd9088e3d1d14c9
Rafael Kitover <rkitover@cpan.org>
2010-05-20 14:28:18 +0000

    support INSERT OR UPDATE triggers for Oracle

--- lib/DBIx/Class/Storage/DBI/Oracle/Generic.pm
+++ lib/DBIx/Class/Storage/DBI/Oracle/Generic.pm
@@ -85,7 +85,7 @@ sub _dbh_get_autoinc_seq {
     {
       $schema ? (owner => $schema) : (),
       table_name => $table || $source_name,
-      triggering_event => 'INSERT',
+      triggering_event => { -like => '%INSERT%' },
       status => 'ENABLED',
      },
   );
