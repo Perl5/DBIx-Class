e3194e31ae2f6047668d453f958d32f2a67ba6d8
Peter Rabbitson <ribasushi@cpan.org>
2010-06-19 11:38:51 +0200

    Mibbit changed their single-sign-on link bleh!

--- lib/DBIx/Class.pm
+++ lib/DBIx/Class.pm
@@ -64,7 +64,7 @@ The community can be found via:
 =item * IRC: irc.perl.org#dbix-class
 
 =for html
-<a href="http://mibbit.com/chat/#dbix-class@irc.perl.org">(click for instant chatroom login)</a>
+<a href="http://chat.mibbit.com/#dbix-class@irc.perl.org">(click for instant chatroom login)</a>
 
 =item * Mailing list: L<http://lists.scsys.co.uk/mailman/listinfo/dbix-class>
 
