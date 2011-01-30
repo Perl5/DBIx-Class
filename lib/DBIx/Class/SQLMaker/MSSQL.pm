d42d490603dda3c84ab88eebbcac9db4c2cb2b37
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2011-01-30 10:42:57 -0600

    MSSQL implementation of -dt_get

--- lib/DBIx/Class/SQLMaker/MSSQL.pm
+++ lib/DBIx/Class/SQLMaker/MSSQL.pm
@@ -10,4 +10,16 @@ sub _rno_default_order {
   return \ '(SELECT(1))';
 }
 
+{
+  my %part_map = (
+     month        => 'mm',
+     day_of_month => 'dd',
+     year         => 'yyyy',
+  );
+
+  sub _datetime_sql { "DATEPART('$part_map{$_[1]}', $_[2])" }
+  sub _datetime_diff_sql { "DATEDIFF('$part_map{$_[1]}', $_[2], $_[3])" }
+}
+
+
 1;
