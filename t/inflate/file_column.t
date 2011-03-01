Perl5/DBIx-Class

commit 123df5269e2a19f93ff072bbf92832fb23d19f4c

Move the test bits related to deprecated IC::File out of the main testschema

t/inflate/file_column.t
@@ -4,17 +4,39 @@ use warnings;
 use Test::More;
 use lib qw(t/lib);
 
-# inject IC::File into the result baseclass for testing
-BEGIN {
-  $ENV{DBIC_IC_FILE_NOWARN} = 1;
-  require DBICTest::BaseResult;
-  DBICTest::BaseResult->load_components (qw/InflateColumn::File/);
-}
-
 
 use DBICTest;
+use DBICTest::Schema;
 use File::Compare;
 use Path::Class qw/file/;
+
+{
+  local $ENV{DBIC_IC_FILE_NOWARN} = 1;
+
+  package DBICTest::Schema::FileColumn;
+
+  use strict;
+  use warnings;
+  use base qw/DBICTest::BaseResult/;
+
+  use File::Temp qw/tempdir/;
+
+  __PACKAGE__->load_components (qw/InflateColumn::File/);
+  __PACKAGE__->table('file_columns');
+
+  __PACKAGE__->add_columns(
+    id => { data_type => 'integer', is_auto_increment => 1 },
+    file => {
+      data_type        => 'varchar',
+      is_file_column   => 1,
+      file_column_path => tempdir(CLEANUP => 1),
+      size             => 255
+    }
+  );
+
+  __PACKAGE__->set_primary_key('id');
+}
+DBICTest::Schema->load_classes('FileColumn');
 
 my $schema = DBICTest->init_schema;
 

t/lib/DBICTest/Schema.pm
@@ -11,7 +11,6 @@ __PACKAGE__->load_classes(qw/
   BindType
   Employee
   CD
-  FileColumn
   Genre
   Bookmark
   Link

t/lib/DBICTest/Schema/FileColumn.pm
@@ -1,23 +1,0 @@
-package 
-DBICTest::Schema::FileColumn;
-
-use strict;
-use warnings;
-use base qw/DBICTest::BaseResult/;
-use File::Temp qw/tempdir/;
-
-__PACKAGE__->table('file_columns');
-
-__PACKAGE__->add_columns(
-  id => { data_type => 'integer', is_auto_increment => 1 },
-  file => {
-    data_type        => 'varchar',
-    is_file_column   => 1,
-    file_column_path => tempdir(CLEANUP => 1),
-    size             => 255
-  }
-);
-
-__PACKAGE__->set_primary_key('id');
-
-1;

t/lib/sqlite.sql
@@ -1,6 +1,6 @@
 -- 
 -- Created by SQL::Translator::Producer::SQLite
--- Created on Mon Feb 21 12:10:04 2011
+-- Created on Tue Feb 22 18:42:16 2011
 -- 
 
 --
@@ -57,14 +57,6 @@ CREATE TABLE event (
   varchar_datetime varchar(20),
   skip_inflation datetime,
   ts_without_tz datetime
-);
-
---
--- Table: file_columns
---
-CREATE TABLE file_columns (
-  id INTEGER PRIMARY KEY NOT NULL,
-  file varchar(255) NOT NULL
 );
 
 --

commit 8b40ac1a052d2d0fe6b9dd0219bb77bb469d1cc2

create file_columns table when DBICTEST_SQLT_DEPLOY env var is not set
Breakage introduced in 123df52

t/inflate/file_column.t
@@ -42,6 +42,15 @@ my $schema = DBICTest->init_schema;
 
 plan tests => 10;
 
+if (not $ENV{DBICTEST_SQLT_DEPLOY}) {
+  $schema->storage->dbh->do(<<'EOF');
+  CREATE TABLE file_columns (
+    id INTEGER PRIMARY KEY,
+    file VARCHAR(255)
+  )
+EOF
+}
+
 my $rs = $schema->resultset('FileColumn');
 my $source_file = file(__FILE__);
 my $fname = $source_file->basename;