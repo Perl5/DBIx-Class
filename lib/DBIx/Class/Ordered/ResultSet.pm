6266a81dcdd3ed1e573e33db09eaef4e00db60d1
Devin Austin <dhoss@cpan.org>
2011-04-05 16:41:24 -0600

    added warnings for debug

--- lib/DBIx/Class/Ordered/ResultSet.pm
+++ lib/DBIx/Class/Ordered/ResultSet.pm
@@ -3,10 +3,12 @@ use strict;
 use base qw/DBIx::Class::ResultSet/;
 
 sub update {
+  warn "CALLING UPDATE FROM Ordered::ResultSet";
   shift->update_all(@_);
 }
 
 sub delete {
+  warn "CALLING DELETE FROM Ordered::ResultSet";
   shift->delete_all(@_);
 }
 
