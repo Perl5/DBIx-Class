388a1ec76846c37afb75b80137f21c00cc3a8095
Peter Rabbitson <ribasushi@cpan.org>
2012-11-18 20:59:44 +0100

    While the TODO was solved in b11221ff the source of confusion was not

--- maint/Makefile.PL.inc/51_autogen_README.pl
+++ maint/Makefile.PL.inc/51_autogen_README.pl
@@ -1,3 +1,6 @@
+# When a long-standing branch is updated a README may still inger around
+unlink 'README' if -f 'README';
+
 # Makefile syntax allows adding extra dep-specs for already-existing targets,
 # and simply appends them on *LAST*-come *FIRST*-serve basis.
 # This allows us to inject extra depenencies for standard EUMM targets
