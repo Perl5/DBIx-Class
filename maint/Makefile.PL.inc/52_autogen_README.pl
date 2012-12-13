a0c96f2461b2a46b242b8432deb0ad88880f73cd
Peter Rabbitson <ribasushi@cpan.org>
2012-12-13 11:37:37 +0100

    Make pod generation more palatable to the existing toolchain
    While the installation toolchain seems to understand the concept of
    PMLIBDIRS/PMLIBPARENTDIRS rather well, it turns out that both s.c.o.
    and metacpan hav great troubles displaying the results. While there
    is work underway to fix metacpan, the intermittency of the failures
    prompted a rethink of how to handle all of this.
    
    So instead we turn (ab)use of ExtUtils::MakeMaker to 11. The generation
    scripts still dump their contents into .generated_pod/ as before.
    However afterwards we hook the create_distdir EUMM target and split it
    into several targets:
    
    create_distdir : clonedir_generate_files clonedir_post_generate_files fresh_manifest create_distdir_copy_manifested clonedir_cleanup_generated_files
    
    Then we use the pre-existing EUMM install facility to copy the generated
    POD directly to lib/, create a manifest and a distdir, and then delete
    everything we just copied from lib/, leaving the clone-dir as pristine
    as it was before we started.

--- maint/Makefile.PL.inc/52_autogen_README.pl
+++ maint/Makefile.PL.inc/52_autogen_README.pl
@@ -7,17 +7,17 @@ unlink 'README' if -f 'README';
 
 postamble <<"EOP";
 
-.PHONY: dbic_clonedir_cleanup_readme dbic_clonedir_gen_readme
+clonedir_generate_files : dbic_clonedir_gen_readme
 
-distdir : dbic_clonedir_cleanup_readme
-
-create_distdir : dbic_clonedir_gen_readme
+clonedir_cleanup_generated_files : dbic_clonedir_cleanup_readme
 
 dbic_clonedir_gen_readme :
 \tpod2text lib/DBIx/Class.pm > README
 
 dbic_clonedir_cleanup_readme :
 \t\$(RM_F) README
+
+realclean :: dbic_clonedir_cleanup_readme
 
 EOP
 
--- maint/Makefile.PL.inc/53_autogen_pod.pl
+++ maint/Makefile.PL.inc/53_autogen_pod.pl
@@ -8,7 +8,8 @@ my $ver = Meta->version;
 # cleanup the generated pod dir (again - kill leftovers from old checkouts)
 require File::Path;
 require File::Glob;
-File::Path::rmtree( [File::Glob::bsd_glob "$pod_dir/*"], { verbose => 0 } );
+File::Path::rmtree( File::Glob::bsd_glob("$pod_dir/*"), { verbose => 0 } );
+
 
 # generate the OptDeps pod both in the clone-dir and during the makefile distdir
 {
@@ -18,15 +19,14 @@ File::Path::rmtree( [File::Glob::bsd_glob "$pod_dir/*"], { verbose => 0 } );
 
   postamble <<"EOP";
 
-.PHONY: dbic_clonedir_gen_optdeps_pod
-
-create_distdir : dbic_clonedir_gen_optdeps_pod
+clonedir_generate_files : dbic_clonedir_gen_optdeps_pod
 
 dbic_clonedir_gen_optdeps_pod :
-\t\$(ABSPERL) -Ilib -MDBIx::Class::Optional::Dependencies -e "DBIx::Class::Optional::Dependencies->_gen_pod(qw($ver $pod_dir))"
+\t\$(ABSPERLRUN) -Ilib -MDBIx::Class::Optional::Dependencies -e "DBIx::Class::Optional::Dependencies->_gen_pod(qw($ver $pod_dir))"
 
 EOP
 }
+
 
 # generate the inherit pods both in the clone-dir and during the makefile distdir
 {
@@ -37,12 +37,38 @@ EOP
 
   postamble <<"EOP";
 
-.PHONY: dbic_clonedir_gen_inherit_pods
-
-create_distdir : dbic_clonedir_gen_inherit_pods
+clonedir_generate_files : dbic_clonedir_gen_inherit_pods
 
 dbic_clonedir_gen_inherit_pods :
-\t\$(ABSPERL) -Ilib maint/gen_pod_inherit
+\t\$(ABSPERLRUN) -Ilib maint/gen_pod_inherit
+
+EOP
+}
+
+
+# copy the contents of .generated_pod over to lib/
+# (yes, overwriting is fine, though nothing should reside there)
+{
+  postamble <<"EOP";
+
+clonedir_post_generate_files : dbic_clonedir_copy_generated_pod
+
+dbic_clonedir_copy_generated_pod :
+\t\$(RM_F) .generated_pod/.packlist
+\t\$(ABSPERLRUN) -MExtUtils::Install -e 'install([ from_to => {qw(.generated_pod/DBIx lib/DBIx write .generated_pod/.packlist)}, verbose => 0, uninstall_shadows => 0, skip => [] ]);'
+
+EOP
+}
+
+
+# everything that came from .generated_pod, needs to be removed from our lib/
+{
+  postamble <<"EOP";
+
+clonedir_cleanup_generated_files : dbic_clonedir_cleanup_generated_pod_copies
+
+dbic_clonedir_cleanup_generated_pod_copies :
+\t\$(ABSPERLRUN) -MExtUtils::Install -e 'uninstall(qw(.generated_pod/.packlist))'
 
 EOP
 }
