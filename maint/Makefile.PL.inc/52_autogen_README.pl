97867713b03f3f843c22f8723b902ee5bdd7e0a9
Peter Rabbitson <ribasushi@cpan.org>
2012-12-17 09:07:02 +0100

    Move generated README to maint/.Generated_Pod as well - simplifies cleanup

--- maint/Makefile.PL.inc/52_autogen_README.pl
+++ maint/Makefile.PL.inc/52_autogen_README.pl
@@ -5,19 +5,17 @@ unlink 'README' if -f 'README';
 # and simply appends them on *LAST*-come *FIRST*-serve basis.
 # This allows us to inject extra depenencies for standard EUMM targets
 
+require File::Spec;
+my $dir = File::Spec->catdir(qw(maint .Generated_Pod));
+my $fn = File::Spec->catfile($dir, 'README');
+
 postamble <<"EOP";
 
 clonedir_generate_files : dbic_clonedir_gen_readme
 
-clonedir_cleanup_generated_files : dbic_clonedir_cleanup_readme
-
 dbic_clonedir_gen_readme :
-\tpod2text lib/DBIx/Class.pm > README
-
-dbic_clonedir_cleanup_readme :
-\t\$(RM_F) README
-
-realclean :: dbic_clonedir_cleanup_readme
+\t@{[ $mm_proto->oneliner('mkpath', ['-MExtUtils::Command']) ]} $dir
+\tpod2text lib/DBIx/Class.pm > $fn
 
 EOP
 
