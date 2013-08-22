Perl5/DBIx-Class

commit e8c31aecd4b3c6bc4686885c4474c96dd7524716

fix pod for unicode name

lib/DBIx/Class/Storage/DBI/Replicated.pm
@@ -20,6 +20,8 @@ use Try::Tiny;
 
 use namespace::clean -except => 'meta';
 
+=encoding utf8
+
 =head1 NAME
 
 DBIx::Class::Storage::DBI::Replicated - BETA Replicated database support

commit f8213ab0b684ec79699406e21a8fc61512591017

Fix name mangle in Replicated.pm broken by e8c31aec and then by 4a0eed52

.mailmap
@@ -21,6 +21,7 @@ Gordon Irving <goraxe@cpan.org>             <goraxe@goraxe.me.uk>
 Hakim Cassimally <osfameron@cpan.org>       <hakim@vm-participo.(none)>
 Jonathan Chu <milki@rescomp.berkeley.edu>   <milki@rescomp.berkeley.edu>
 Matt Phillips <mattp@cpan.org>              <mphillips@oanda.com>
+Norbert Csongrádi <bert@cpan.org>           <bert@cpan.org>
 Roman Filippov <romanf@cpan.org>            <moltar@moltar.net>
 Peter Rabbitson <ribasushi@cpan.org>        <rabbit@viator.rabbit.us>
 Tim Bunce <Tim.Bunce@pobox.com>             <Tim.Bunce@ig.co.uk>

lib/DBIx/Class.pm
@@ -348,7 +348,7 @@ arcanez: Justin Hunter <justin.d.hunter@gmail.com>
 
 ash: Ash Berlin <ash@cpan.org>
 
-bert: Norbert Csongradi <bert@cpan.org>
+bert: Norbert Csongrádi <bert@cpan.org>
 
 blblack: Brandon L. Black <blblack@gmail.com>
 
@@ -491,6 +491,8 @@ penguin: K J Cheetham
 perigrin: Chris Prather <chris@prather.org>
 
 peter: Peter Collingbourne <peter@pcc.me.uk>
+
+Peter Siklósi <einon@einon.hu>
 
 Peter Valdemar ME<oslash>rch <peter@morch.com>
 

lib/DBIx/Class/Storage/DBI/Replicated.pm
@@ -1117,8 +1117,8 @@ using the Schema clone method.
 
 Based on code originated by:
 
-  Norbert Csongr�di <bert@cpan.org>
-  Peter Sikl�si <einon@einon.hu>
+  Norbert Csongrádi <bert@cpan.org>
+  Peter Siklósi <einon@einon.hu>
 
 =head1 LICENSE
 