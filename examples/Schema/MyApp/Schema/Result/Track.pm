20bfea3fd35252310456e125e8dfe948f6c675c2
Peter Rabbitson <ribasushi@cpan.org>
2013-08-26 13:10:59 +0200

    Update example Result classes for proper DDL generation
    Also given that this is one of the first things a newbie sees, it is prudent
    to stick to canonical definition

--- examples/Schema/MyApp/Schema/Result/Track.pm
+++ examples/Schema/MyApp/Schema/Result/Track.pm
@@ -3,13 +3,26 @@ package MyApp::Schema::Result::Track;
 use warnings;
 use strict;
 
-use base qw/DBIx::Class::Core/;
+use base qw( DBIx::Class::Core );
 
 __PACKAGE__->table('track');
 
-__PACKAGE__->add_columns(qw/ trackid cd title/);
+__PACKAGE__->add_columns(
+  trackid => {
+    data_type => 'integer',
+    is_auto_increment => 1
+  },
+  cd => {
+    data_type => 'integer',
+  },
+  title => {
+    data_type => 'text',
+  },
+);
 
 __PACKAGE__->set_primary_key('trackid');
+
+__PACKAGE__->add_unique_constraint([qw( title cd )]);
 
 __PACKAGE__->belongs_to('cd' => 'MyApp::Schema::Result::Cd');
 
