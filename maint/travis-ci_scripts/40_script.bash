ad191fcfeff95660cf13d2f8bfb7ad5162c93039
Peter Rabbitson <ribasushi@cpan.org>
2013-09-08 18:04:17 +0200

    Only run our own tests concurrently, not stuff in after_success

--- maint/travis-ci_scripts/40_script.bash
+++ maint/travis-ci_scripts/40_script.bash
@@ -3,15 +3,19 @@
 source maint/travis-ci_scripts/common.bash
 if [[ -n "$SHORT_CIRCUIT_SMOKE" ]] ; then return ; fi
 
-export HARNESS_TIMER=1 HARNESS_OPTIONS=c:j$NUMTHREADS
+run_harness_tests() {
+  local -x HARNESS_TIMER=1
+  local -x HARNESS_OPTIONS=c:j$NUMTHREADS
+  make test 2> >(tee "$TEST_STDERR_LOG")
+}
 
 TEST_T0=$SECONDS
 if [[ "$CLEANTEST" = "true" ]] ; then
   echo_err "$(tstamp) Running tests with plain \`make test\`"
   run_or_err "Prepare blib" "make pure_all"
-  make test 2> >(tee "$TEST_STDERR_LOG")
+  run_harness_tests
 else
-  PROVECMD="prove -lrswj$NUMTHREADS t xt"
+  PROVECMD="prove --timer -lrswj$NUMTHREADS t xt"
   echo_err "$(tstamp) running tests with \`$PROVECMD\`"
   $PROVECMD 2> >(tee "$TEST_STDERR_LOG")
 fi
