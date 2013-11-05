d8541ca37385f6d112bf251c0c8ea6a628abcc9b
Peter Rabbitson <ribasushi@cpan.org>
2013-11-05 09:26:23 +0100

    This is an expensive author-only test, move to xt/
    Also get xt tests first in line (won't matter much with -s)

--- maint/travis-ci_scripts/40_script.bash
+++ maint/travis-ci_scripts/40_script.bash
@@ -14,7 +14,7 @@ if [[ "$CLEANTEST" = "true" ]] ; then
   run_or_err "Prepare blib" "make pure_all"
   run_harness_tests
 else
-  PROVECMD="prove -lrswj$NUMTHREADS t xt"
+  PROVECMD="prove -lrswj$NUMTHREADS xt t"
 
   # FIXME - temporary, until Package::Stash is fixed
   if perl -M5.010 -e 1 &>/dev/null ; then
