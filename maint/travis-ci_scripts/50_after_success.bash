e5ba0859050c5e14938527ceb85c1f30e501e0f4
Peter Rabbitson <ribasushi@cpan.org>
2013-12-07 08:23:37 +0100

    New Test::Kwalitee completely prevents install of Pod::POM
    Just skip all tests until we get a new release

--- maint/travis-ci_scripts/50_after_success.bash
+++ maint/travis-ci_scripts/50_after_success.bash
@@ -4,7 +4,7 @@ source maint/travis-ci_scripts/common.bash
 if [[ -n "$SHORT_CIRCUIT_SMOKE" ]] ; then return ; fi
 
 if [[ "$CLEANTEST" != "true" ]] ; then
-  run_or_err "Install Pod::POM separately via cpan" "cpan Pod::POM"
+  run_or_err "Install Pod::POM separately via cpan" "cpan -f -i Pod::POM || perl -MPod::POM -e 1"
   parallel_installdeps_notest $(perl -Ilib -MDBIx::Class -e 'print join " ", keys %{DBIx::Class::Optional::Dependencies->req_list_for("dist_dir")}')
   run_or_err "Attempt to build a dist with all prereqs present" "make dist"
 fi
