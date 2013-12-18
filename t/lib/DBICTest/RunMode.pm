6e22e629bb00f5c721a6fbec0017596c1e28dde3
Karen Etheridge <ether@cpan.org>
2013-12-18 12:41:27 -0800

    misspelling

--- t/lib/DBICTest/RunMode.pm
+++ t/lib/DBICTest/RunMode.pm
@@ -106,7 +106,7 @@ to ensure you have all required dependencies present. Not doing
 so often results in a lot of wasted time for other contributors
 trying to assist you with spurious "its broken!" problems.
 
-By default DBICs Makefile.PL turns all optional dependenciess into
+By default DBICs Makefile.PL turns all optional dependencies into
 *HARD REQUIREMENTS*, in order to make sure that the entire test
 suite is executed, and no tests are skipped due to missing modules.
 If you for some reason need to disable this behavior - supply the
