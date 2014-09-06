8f7ebb093248d0b8393bff963c5aeb1c3dced2b2
Gareth Kirwan <gbjk@thermeon.com>
2014-09-06 17:14:49 +0000

    Fix connect vivifying all sources anyway

--- lib/DBIx/Class/Schema.pm
+++ lib/DBIx/Class/Schema.pm
@@ -12,6 +12,7 @@ use DBIx::Class::_Util 'refcount';
 use Sub::Name 'subname';
 use Devel::GlobalDestruction;
 use namespace::clean;
+use Module::Loaded;
 
 __PACKAGE__->mk_classdata('class_mappings' => {});
 __PACKAGE__->mk_classdata('source_registrations' => {});
@@ -1063,9 +1064,16 @@ sub _copy_state_from {
   my ($self, $from) = @_;
 
   $self->class_mappings({ %{$from->class_mappings} });
-  $self->source_registrations({ %{$from->source_registrations} });
+
+  my $sregs = { %{$from->source_registrations} };
+  $self->source_registrations($sregs);
 
   foreach my $source_name ($from->sources) {
+
+    # Skip any source that isn't yet created
+    # This allows lazy to work past connect
+    next unless is_loaded($source_name);
+
     my $source = $from->source($source_name);
     my $new = $source->new($source);
     # we use extra here as we want to leave the class_mappings as they are
