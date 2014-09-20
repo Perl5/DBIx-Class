3437fbe9986916a555b7c924c74be1de762702d8
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2014-09-20 13:24:00 +0100

    Add basic .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((cperl-mode . ((cperl-indent-level . 2)
+                (cperl-continued-statement-offset . 2)
+                (cperl-continued-brace-offset . 0)
+                (cperl-close-paren-offset . -2)
+                (cperl-indent-subs-specially . nil)
+                (indent-tabs-mode . nil))))
