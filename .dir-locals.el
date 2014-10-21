3f6a394fe6ffd511796085e22d2651ef04ea70ef
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2014-10-21 22:48:15 +0100

    Make sure emacs doesn't try to indent with tabs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
-((cperl-mode . ((cperl-indent-level . 2)
+((nil . ((indent-tabs-mode . nil)))
+ (cperl-mode . ((cperl-indent-level . 2)
                 (cperl-continued-statement-offset . 2)
                 (cperl-continued-brace-offset . 0)
                 (cperl-close-paren-offset . -2)
