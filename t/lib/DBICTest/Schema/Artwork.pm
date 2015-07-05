b79eae3bd2b5abeb6789cf5e35f592cdaba92482
Peter Rabbitson <ribasushi@cpan.org>
2015-07-05 12:16:46 +0200

    Expand (and complicate) m2m test from e98e6478
    There are no logical changes, the only real addition is the
    extra opaque relcond in the middle

--- t/lib/DBICTest/Schema/Artwork.pm
+++ t/lib/DBICTest/Schema/Artwork.pm
@@ -21,12 +21,10 @@ __PACKAGE__->has_many('images', 'DBICTest::Schema::Image', 'artwork_id');
 __PACKAGE__->has_many('artwork_to_artist', 'DBICTest::Schema::Artwork_to_Artist', 'artwork_cd_id');
 __PACKAGE__->many_to_many('artists', 'artwork_to_artist', 'artist');
 
-# both to test manytomany with custom rel
-# (deliberate misnamed accessor clash)
-__PACKAGE__->many_to_many('artist_limited_rank', 'artwork_to_artist', 'artist_limited_rank');
-__PACKAGE__->many_to_many('artist_limited_rank_opaque', 'artwork_to_artist', 'artist_limited_rank_opaque');
+# both to test manytomany via double custom rel (deliberate misnamed accessor clash)
+__PACKAGE__->many_to_many('artist_limited_rank', 'artwork_to_artist_via_customcond', 'artist_limited_rank');
+__PACKAGE__->many_to_many('artist_limited_rank_opaque', 'artwork_to_artist_via_opaque_customcond', 'artist_limited_rank_opaque');
 
-# other test to manytomany
 __PACKAGE__->has_many('artwork_to_artist_via_customcond', 'DBICTest::Schema::Artwork_to_Artist',
   sub {
     # This is for test purposes only. A regular user does not
@@ -43,7 +41,21 @@ __PACKAGE__->has_many('artwork_to_artist_via_customcond', 'DBICTest::Schema::Art
     );
   }
 );
-__PACKAGE__->many_to_many('artists_via_customcond', 'artwork_to_artist_via_customcond', 'artist');
+
+__PACKAGE__->has_many('artwork_to_artist_via_opaque_customcond', 'DBICTest::Schema::Artwork_to_Artist',
+  sub {
+    # This is for test purposes only. A regular user does not
+    # need to sanity check the passed-in arguments, this is what
+    # the tests are for :)
+    my $args = &check_customcond_args;
+
+    return (
+      { "$args->{foreign_alias}.artwork_cd_id" => { -ident => "$args->{self_alias}.cd_id" } },
+    );
+  }
+);
+
+__PACKAGE__->many_to_many('all_artists_via_opaque_customcond', 'artwork_to_artist_via_opaque_customcond', 'artist');
 
 
 1;
