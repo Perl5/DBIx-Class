Perl5/DBIx-Class

commit 4f52479b6963cb1e61e28c849c0d1e47a3c57087

Fix endless loop on BareSourcelessResultClass->throw_exception(...)
There is a better fix for this in the pipes, but this will do for now

lib/DBIx/Class/Row.pm
@@ -1564,8 +1564,8 @@ See L<DBIx::Class::Schema/throw_exception>.
 sub throw_exception {
   my $self=shift;
 
-  if (ref $self && ref $self->result_source ) {
-    $self->result_source->throw_exception(@_)
+  if (ref $self && ref (my $rsrc = try { $self->result_source_instance } ) ) {
+    $rsrc->throw_exception(@_)
   }
   else {
     DBIx::Class::Exception->throw(@_);

t/60core.t
@@ -636,4 +636,6 @@ SKIP: {
 
 throws_ok { $schema->resultset} qr/resultset\(\) expects a source name/, 'resultset with no argument throws exception';
 
+throws_ok { $schema->source('Artist')->result_class->new( 'bugger' ) } qr/must be a hashref/;
+
 done_testing;

t/resultsource/bare_resultclass_exception.t
@@ -1,0 +1,17 @@
+use strict;
+use warnings;
+
+use Test::More;
+use Test::Exception;
+
+use lib 't/lib';
+use DBICTest;
+
+{
+  package DBICTest::Foo;
+  use base "DBIx::Class::Core";
+}
+
+throws_ok { DBICTest::Foo->new("urgh") } qr/must be a hashref/;
+
+done_testing;

commit 78de6edd28ca54c3025eb1b7407f2cb43865ce18

Augment the infinite loop fix 4f52479b with the infra of ddcc02d1
This also fixes a manifestation (but not the cause, this comes later) of a
bug where $result->result_source != $result->result_source_instance.

This commit is made early as it is rather efficient anyway, and it allows me
to test Handel (which was broken by 4f52479b) to validate subsequent rsrc
rewritering

lib/DBIx/Class/Row.pm
@@ -1585,9 +1585,9 @@ sub throw_exception {
   my $self=shift;
 
   if (
-    ref $self
+    ! DBIx::Class::_Util::in_internal_try
       and
-    my $rsrc = dbic_internal_try { $self->result_source_instance }
+    my $rsrc = dbic_internal_try { $self->result_source }
   ) {
     $rsrc->throw_exception(@_)
   }