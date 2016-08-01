c33d5ebc4d84e4338f269565f6fe011801cb9fd4
Peter Rabbitson <ribasushi@cpan.org>
2016-08-01 18:43:44 +0200

    Bump version to accomodate pre-beta testers

--- lib/DBIx/Class.pm
+++ lib/DBIx/Class.pm
@@ -14,7 +14,7 @@ our $VERSION;
 # $VERSION declaration must stay up here, ahead of any other package
 # declarations, as to not confuse various modules attempting to determine
 # this ones version, whether that be s.c.o. or Module::Metadata, etc
-$VERSION = '0.082899_15';
+$VERSION = '0.082899_25';
 
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
 
