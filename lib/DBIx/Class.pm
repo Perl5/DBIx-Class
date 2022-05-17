0373da7e7e4eeb7953299d9d4080f2ad24ba103d
Peter Rabbitson <ribasushi@leporine.io>
2022-05-17 12:48:54 +0200

    Release v0.082843

--- lib/DBIx/Class.pm
+++ lib/DBIx/Class.pm
@@ -11,7 +11,7 @@ our $VERSION;
 # $VERSION declaration must stay up here, ahead of any other package
 # declarations, as to not confuse various modules attempting to determine
 # this ones version, whether that be s.c.o. or Module::Metadata, etc
-$VERSION = '0.082842';
+$VERSION = '0.082843';
 
 {
   package
